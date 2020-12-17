62a0093585f56e2b95165f6d2f4183abe39c7863
andrey.prozorov <andrey.prozorov@arcadia.spb.ru>
2020-12-17 16:32:59 +0300

    Implement proper volumes dependency, implement certbot dependency of node, add non-www cert, add auto reload https server if no cert

--- prod-with-node/server-example.js
+++ prod-with-node/server-example.js
@@ -4,55 +4,86 @@ import * as sapper from '@sapper/server'
 import { redirects } from './redirects'
 
 const proxy = require('express-http-proxy'),
-  https = require('https'),
-  fs = require('fs')
+    https = require('https'),
+    fs = require('fs')
 const express = require('express')
-const expressApp = express() // https
+const httpsApp = express() // https
 const httpApp = express() // http
 
-const { NODE_ENV } = process.env
-const dev = NODE_ENV === 'development'
-
-// no dev http -> https && ignore .well-known
-if (!dev) {
-  httpApp.use(
-    '/.well-known/acme-challenge',
-    express.static(
-      '/var/www/front/static/letsencrypt/.well-known/acme-challenge',
-      { dotfiles: 'allow' }
-    )
-  )
-  httpApp.get(/^((?!.*api.*).)*$/, function(req, res) {
-    res.redirect('https://' + req.headers.host + req.url)
-  })
-}
+const { NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+const prod = !dev;
 
 // inner request network
 httpApp.use(
-  '/api',
-  proxy('http://web', {
-    proxyReqPathResolver: function(req) {
-      return '/wp-json' + req.url
-    },
-  })
+    '/api',
+    proxy('http://web', {
+      proxyReqPathResolver: function(req) {
+        return '/wp-json' + req.url
+      },
+    })
 )
-expressApp.use(
-  '/api',
-  proxy('http://web', {
-    proxyReqPathResolver: function(req) {
-      return '/wp-json' + req.url
-    },
-  })
+httpsApp.use(
+    '/api',
+    proxy('http://web', {
+      proxyReqPathResolver: function(req) {
+        return '/wp-json' + req.url
+      },
+    })
 )
 
 // use http:// for dev mode and use sapper
 if (dev) {
   httpApp.use(
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
+      compression({ threshold: 0 }),
+      sirv('static', { dev }),
+      sapper.middleware()
   )
 }
+
+// on prod http -> https && ignore .well-known
+if (prod) {
+  // http
+  httpApp.use(
+      '/.well-known/acme-challenge',
+      express.static(
+          '/var/www/front/public/static/letsencrypt/.well-known/acme-challenge',
+          { dotfiles: 'allow' }
+      )
+  )
+  httpApp.get(/^.*$/, function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url)
+  })
+
+  httpsApp.get(/.*/, function(req, res, next) {
+    const host = req.header("host");
+    if (host.match(/^www\..*/i)) {
+      next();
+    } else {
+      res.redirect(301, "https://www." + host + req.url);
+    }
+  });
+
+  //https
+  const context = {
+    server: null,
+    endDate: null,
+    failRuns: 0
+  };
+
+
+  // in prod mode prod redirects and use sapper
+  redirects(httpsApp)
+
+  httpsApp.use(
+      compression({ threshold: 0 }),
+      sirv('static', { dev }),
+      sapper.middleware()
+  )
+
+  safeStartHttps(context);
+}
+
 
 // need to be listened here in order to redirect && ignore .well-known in prod
 // and use http:// for dev mode
@@ -61,50 +92,58 @@ httpApp.listen(3000, err => {
   if (err) console.log('error', err)
 })
 
-// in prod mode prod redirects and use sapper
-if (!dev) {
-  redirects(expressApp)
 
-  expressApp.use(
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
-  )
 
-  const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/[[domain_name.com]]/privkey.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/[[domain_name.com]]/chain.pem'),
-    cert: fs.readFileSync(
-      '/etc/letsencrypt/live/[[domain_name.com]]/fullchain.pem'
-    ),
+// util server run logic
+function safeStartHttps(_context) {
+  try {
+
+    const options = {
+      key: fs.readFileSync('/etc/letsencrypt/live/[[domain_name.com]]/privkey.pem'),
+      ca: fs.readFileSync('/etc/letsencrypt/live/[[domain_name.com]]/chain.pem'),
+      cert: fs.readFileSync(
+          '/etc/letsencrypt/live/[[domain_name.com]]/fullchain.pem'
+      ),
+    }
+
+    startHttpsWithAutoRefresh(options, _context);
+
+  } catch (e) {
+
+    _context.failRuns ++;
+    if(_context.failRuns < 20) {
+      console.log(JSON.stringify(e));
+      setTimeout(() => safeStartHttps(_context), 1000 * 60)
+    } else {
+      console.log('no cert, stop trying after 20 attempts');
+    }
+
   }
+}
 
-  let server;
-  let endDate;
-
-  function setEndDate() {
-    endDate = new Date();
-    endDate.setDate((new Date()).getDate() + 30);
-  }
-
-  function startServer() {
-    setEndDate();
-    server && server.close();
-    server = https.createServer(options, expressApp)
-
-    server.listen(3001, function() {
-      // https
-      console.log('https')
-    });
-  }
-
-  startServer();
+function startHttpsWithAutoRefresh(_options, _context) {
+  startServer(_options, _context);
   setInterval(() => {
     let curDate  = new Date();
-    if(endDate && (endDate > curDate)){
+    if(_context.endDate && (_context.endDate > curDate)){
       return;
     }
-    startServer();
+    startServer(_options, _context);
   },  1000 * 60 * 60 * 24 * 5)
+}
 
+function setEndDate(_context) {
+  _context.endDate = new Date();
+  _context.endDate.setDate((new Date()).getDate() + 30);
+}
+
+function startServer(_options, _context) {
+  setEndDate(_context);
+  _context.server && _context.server.close();
+  _context.server = https.createServer(_options, httpsApp)
+
+  _context.server.listen(3001, function() {
+    // https
+    console.log('https started on 3001')
+  });
 }
